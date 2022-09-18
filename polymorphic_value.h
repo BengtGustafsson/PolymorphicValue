a8d5893fa87e0c8a9a70e666d97fde81ef911f7c
Bengt Gustafsson <bengt.gustafsson@beamways.com>
2022-09-18 15:41:24 +0200

    Added test program and a cmakelists

--- polymorphic_value.h
+++ polymorphic_value.h
@@ -32,22 +32,38 @@ Further information at: https://opensource.org/licenses/MIT.
 #include <utility>
 #include <optional>
 
+
+#if IS_STANDARDIZED
+
+#define STD std
+namespace std {
+
+#else
+
+#define STD stdx
+namespace stdx {
+
+using namespace std;
+
+#endif
+
+
 template<typename T, size_t SZ = 64> class polymorphic_value {
 public:
     const static size_t sbo_size = SZ >= sizeof(T) ? SZ : 1;
     polymorphic_value() {}
-    polymorphic_value(std::nullopt_t) {}
-    polymorphic_value(const polymorphic_value& src) requires std::is_copy_constructible_v<T> {
+    polymorphic_value(nullopt_t) {}
+    polymorphic_value(const polymorphic_value& src) requires is_copy_constructible_v<T> {
         src.m_handler.imbue_handler(m_handler);
         src.m_handler.copy(m_data, src.m_data);
     }
-    polymorphic_value(polymorphic_value&& src) requires std::is_move_constructible_v<T> {
+    polymorphic_value(polymorphic_value&& src) requires is_move_constructible_v<T> {
         src.m_handler.imbue_handler(m_handler);
         src.m_handler.move(m_data, src.m_data);
         src.reset();
     }
-    template<typename U, typename... Args> polymorphic_value(std::in_place_type_t<U>, Args&&... args)  requires std::is_base_of_v<T, U> {
-        emplace<U>(std::forward<Args>(args)...);
+    template<typename U, typename... Args> polymorphic_value(in_place_type_t<U>, Args&&... args)  requires is_base_of_v<T, U> {
+        emplace<U>(forward<Args>(args)...);
     }
 
     ~polymorphic_value() {
@@ -57,7 +73,7 @@ public:
     // TODO: To handle the odd case that a T is copy-assignable but not copy-constructible or vice versa
     // handler would actually need to have both copy_assign and copy_contruct. Same for move. 
     // Note that this can only be done if the embedded U of both me and src are the same, otherwise this destroy/construct sequence must be done anyway.
-    polymorphic_value& operator=(const polymorphic_value& src) requires std::is_copy_constructible_v<T> {
+    polymorphic_value& operator=(const polymorphic_value& src) requires is_copy_constructible_v<T> {
         if (this == &src)
             return *this;
 
@@ -67,7 +83,7 @@ public:
         return *this;
     };
 
-    polymorphic_value& operator=(polymorphic_value&& src) requires std::is_move_constructible_v<T> {
+    polymorphic_value& operator=(polymorphic_value&& src) requires is_move_constructible_v<T> {
         if (this == &src)
             return *this;
 
@@ -83,11 +99,11 @@ public:
         m_handler.destroy(m_data);
         if constexpr (sizeof(U) <= sbo_size) {
             new(&m_handler) small_handler<U>;
-            std::construct_at(reinterpret_cast<U*>(m_data.m_bytes), std::forward<Args>(args)...);
+            construct_at(reinterpret_cast<U*>(m_data.m_bytes), forward<Args>(args)...);
         }
         else {
             new(&m_handler) big_handler<U>;
-            std::construct_at(&m_data.m_ptr, std::make_unique<U>(std::forward<Args>(args)...));
+            construct_at(&m_data.m_ptr, make_unique<U>(forward<Args>(args)...));
         }
     }
 
@@ -114,7 +130,7 @@ public:
     T* operator->() { return get(); }
     const T* operator->() const { return get(); }
 
-    // std::optional API
+    // optional API
     // Maybe a holds_alternative<U> from variant is more appropriate? But viewing different subclasses as alternatives seems a bit
     // misleading.
     bool has_value() const { return *this; }
@@ -124,13 +140,13 @@ public:
     template<typename U = T> U& value() {
         U* ret = dynamic_cast<U*>(get());
         if (ret == nullptr)
-            throw std::bad_optional_access();
+            throw bad_optional_access();
         return *ret;
     }
     template<typename U = T> const U& value() const {
         U* ret = dynamic_cast<const U*>(get());
         if (ret == nullptr)
-            throw std::bad_optional_access();
+            throw bad_optional_access();
         return *ret;
     }
     template<typename U = T, typename V> U value_or(V&& default_value) const {
@@ -138,21 +154,21 @@ public:
         if (ret != nullptr)
             return *ret;
         else
-            return static_cast<U>(std::forward<V>(default_value));
+            return static_cast<U>(forward<V>(default_value));
     }
 
     // Note: and_then requires F to return optional<X> or polymorphic_value<X> for some type X.
     // This requirement is from optional, but in fact is quite strange, it could be any return type with a value-constructed
     // default, such as a pointer.
     template<typename U = T, typename F> auto and_then(F&& f) {
-        using R = std::remove_cvref_t<std::invoke_result_t<F, U&>>; // R must be some optional type.
+        using R = remove_cvref_t<invoke_result_t<F, U&>>; // R must be some optional type.
         if (has_value<U>())
             return f(*static_cast<U*>(get()));
         else
             return R();
     }
     template<typename U = T, typename F> auto and_then(F&& f) const {
-        using R = std::remove_cvref_t<std::invoke_result_t<F, const U&>>;
+        using R = remove_cvref_t<invoke_result_t<F, const U&>>;
         if (has_value<U>())
             return f(*static_cast<const U*>(get()));
         else
@@ -161,30 +177,30 @@ public:
 
     // transform wraps the return value of F in an optional.
     template<typename U = T, typename F> auto transform(F&& f) {
-        using R = std::remove_cvref_t<std::invoke_result_t<F, U&>>;
+        using R = remove_cvref_t<invoke_result_t<F, U&>>;
         if (has_value<U>())
-            return std::optional<R>(f(*static_cast<U*>(get())));
+            return optional<R>(f(*static_cast<U*>(get())));
         else
-            return std::optional<R>();
+            return optional<R>();
     }
     template<typename U = T, typename F> auto transform(F&& f) const {
-        using R = std::remove_cvref_t<std::invoke_result_t<F, const U&>>;
+        using R = remove_cvref_t<invoke_result_t<F, const U&>>;
         if (has_value<U>())
-            return std::optional<R>(f(*static_cast<const U*>(get())));
+            return optional<R>(f(*static_cast<const U*>(get())));
         else
-            return std::optional<R>();
+            return optional<R>();
     }
 
     // or_else requires F to return some optional.
     template<typename U = T, typename F> auto or_else(F&& f) {
-        using R = std::remove_cvref_t<std::invoke_result_t<F>>;
+        using R = remove_cvref_t<invoke_result_t<F>>;
         if (has_value<U>())
             return R(*static_cast<U*>(get()));
         else
             return f();
     }
     template<typename U = T, typename F> auto or_else(F&& f) const {
-        using R = std::remove_cvref_t<std::invoke_result_t<F>>;
+        using R = remove_cvref_t<invoke_result_t<F>>;
         if (has_value<U>())
             return R(*static_cast<const U*>(get()));
         else
@@ -196,8 +212,8 @@ private:
         data() : m_ptr(nullptr) {}
         ~data() {}
 
-        std::byte m_bytes[sbo_size];
-        std::unique_ptr<T> m_ptr;
+        byte m_bytes[sbo_size];
+        unique_ptr<T> m_ptr;
     };
 
     // Note: handler_base is not abstract, instead it is used for empty objects.
@@ -219,20 +235,20 @@ private:
         const T* get(const data& d) const override { return static_cast<const T*>(reinterpret_cast<const U*>(d.m_bytes)); }
 
         void copy(data& dest, const data& src) const override {
-            if constexpr (std::is_copy_constructible_v<U>)
-                std::construct_at<U>(reinterpret_cast<U*>(dest.m_bytes), *reinterpret_cast<const U*>(src.m_bytes));
+            if constexpr (is_copy_constructible_v<U>)
+                construct_at<U>(reinterpret_cast<U*>(dest.m_bytes), *reinterpret_cast<const U*>(src.m_bytes));
             else
-                throw std::runtime_error("Tried to copy an object of move only subclass in a polymorphic_value<T> where T is copyable");
+                throw runtime_error("Tried to copy an object of move only subclass in a polymorphic_value<T> where T is copyable");
         }
         
         void move(data& dest, data& src) const override {
-            if constexpr (std::is_move_constructible_v<U>)
-                std::construct_at<U>(reinterpret_cast<U*>(dest.m_bytes), std::move(*reinterpret_cast<U*>(src.m_bytes)));
+            if constexpr (is_move_constructible_v<U>)
+                construct_at<U>(reinterpret_cast<U*>(dest.m_bytes), std::move(*reinterpret_cast<U*>(src.m_bytes)));
             else
-                throw std::runtime_error("Tried to move an object of immovable subclass in a polymorphic_value<T> where T is movable");
+                throw runtime_error("Tried to move an object of immovable subclass in a polymorphic_value<T> where T is movable");
         }
 
-        void destroy(data& d) const override { std::destroy_at(reinterpret_cast<U*>(d.m_bytes)); }
+        void destroy(data& d) const override { destroy_at(reinterpret_cast<U*>(d.m_bytes)); }
     };
     
     template<typename U> struct big_handler final : public handler_base {
@@ -242,16 +258,16 @@ private:
         const T* get(const data& d) const override { return d.m_ptr.get(); }
 
         void copy(data& dest, const data& src) const override { 
-            if constexpr (std::is_copy_constructible_v<U>)
-                std::construct_at(&dest.m_ptr, std::make_unique<U>(static_cast<const U&>(*src.m_ptr)));
+            if constexpr (is_copy_constructible_v<U>)
+                construct_at(&dest.m_ptr, make_unique<U>(static_cast<const U&>(*src.m_ptr)));
             else
-                throw std::runtime_error("Tried to copy an object of move only subclass in a polymorphic_value<T> where T is copyable");
+                throw runtime_error("Tried to copy an object of move only subclass in a polymorphic_value<T> where T is copyable");
         }
         void move(data& dest, data& src) const override {
-            if constexpr (std::is_move_constructible_v<U>)
-                std::construct_at(&dest.m_ptr, std::move(src.m_ptr));
+            if constexpr (is_move_constructible_v<U>)
+                construct_at(&dest.m_ptr, std::move(src.m_ptr));
             else
-                throw std::runtime_error("Tried to move an object of immovable subclass in a polymorphic_value<T> where T is movable");
+                throw runtime_error("Tried to move an object of immovable subclass in a polymorphic_value<T> where T is movable");
         }
 
         void destroy(data& d) const override { d.m_ptr = nullptr; }
@@ -263,10 +279,12 @@ private:
 
 template<typename T, typename U = T, typename... Args> polymorphic_value<T> make_polymorphic_value(Args&&... args)
 {
-    return polymorphic_value<T>(std::in_place_type<U>, std::forward<Args>(args)...);
+    return polymorphic_value<T>(in_place_type<U>, forward<Args>(args)...);
 }
 
 template<typename T, size_t SZ, typename U = T, typename... Args> polymorphic_value<T, SZ> make_polymorphic_value(Args&&... args)
 {
-    return polymorphic_value<T, SZ>(std::in_place_type<U>, std::forward<Args>(args)...);
+    return polymorphic_value<T, SZ>(in_place_type<U>, forward<Args>(args)...);
 }
+
+}       // Namespace std or stdx
---
+++ test_polymorphic_value.cpp
@@ -0,0 +1,135 @@
+#include "polymorphic_value.h"
+
+#include <cassert>
+#include <iostream>
+
+struct SmallBase {
+    virtual ~SmallBase() {}
+    virtual void identify() { std::cout << "SmallBase" << std::endl; }
+    int x = 17;
+};
+
+struct SmallSub : public SmallBase {
+    SmallSub(int y) : y(y) {}
+    
+    void identify() { std::cout << "SmallSub " << y << std::endl; }
+    int y;
+};
+
+struct BigSub : public SmallBase {
+    void identify() { std::cout << "BigSub" << std::endl; }
+    int y[100];
+};
+
+#if IS_STANDARDIZED
+using namespace std;
+#else
+using namespace stdx;
+#endif
+
+static_assert(sizeof(polymorphic_value<SmallBase>) == 64 + sizeof(void*));
+static_assert(sizeof(polymorphic_value<SmallBase, 2>) == 2 * sizeof(void*));
+static_assert(sizeof(polymorphic_value<BigSub>) == 2 * sizeof(void*));
+static_assert(sizeof(polymorphic_value<BigSub, 512>) == 512 + sizeof(void*));
+
+struct MoveOnly : public SmallBase {
+    MoveOnly() = default;
+    MoveOnly(const MoveOnly&) = delete;
+    MoveOnly(MoveOnly&&) = default;
+    MoveOnly& operator=(const MoveOnly&) = delete;
+    MoveOnly& operator=(MoveOnly&) = default;
+};
+
+
+int main()
+{
+    polymorphic_value<SmallBase> sv;
+    assert(!sv);
+    sv.emplace<SmallSub>(1);
+    assert(sv);
+    assert(sv.has_value<SmallSub>());
+
+    sv->identify(); // 1
+
+    sv.value<SmallSub>().y = 2;
+
+    std::optional<int> o0 = sv.transform<SmallSub>([](SmallSub& ss) { return ss.y + 1; });
+
+    assert(o0 && *o0 == 3);
+    
+    auto sv2 = sv;
+    sv2->identify();    // 2
+    sv->identify();     // 2
+
+    sv2.emplace<BigSub>();
+    sv2->identify();
+    sv = std::move(sv2);
+    sv->identify();
+    assert(!sv2);
+    
+    std::optional<int> o1 = sv2.transform<SmallSub>([](SmallSub& ss) { return ss.y + 1; });
+    assert(!o1);
+    o1 = sv.transform<SmallSub>([](SmallSub& ss) { return ss.y + 1; });
+    assert(!o1);        // Still not ok as sv is a BigSub.
+
+    SmallBase v1 = sv.value();
+    SmallSub v2 = sv2.value_or<SmallSub>(SmallSub(7));
+    assert(v2.y == 7);      // Default value as sv2 is empty.
+
+    sv.emplace<SmallSub>(5);
+    v2 = sv.value_or<SmallSub>(SmallSub(7));
+    assert(v2.y == 5);  // Default value not used as sv contains a SmallSub
+    
+    // Test and_then with a function returning optional
+    std::optional<int> o2 = sv.and_then<SmallSub>([](SmallSub& ss) { return std::optional(ss.y + 1); });
+    assert(o2 && *o2 == 6);
+
+    // Test and_then with a function returning polymorphic_value. This just clears the value if it contained BigSub:
+    sv = sv.and_then([](SmallBase& v) {
+        if (dynamic_cast<SmallSub*>(&v) == nullptr)
+            return polymorphic_value<SmallBase>();
+        else
+            return polymorphic_value<SmallBase>(std::in_place_type<SmallSub>, static_cast<SmallSub&>(v));
+    });
+    assert(sv.has_value<SmallSub>());
+    
+    std::optional<BigSub> o3 = sv.or_else<BigSub>([]() { return std::optional<BigSub>(); });
+    assert(!o3);
+
+    // Test in_place construction
+    polymorphic_value<SmallBase> sv3(std::in_place_type<SmallSub>, 4);
+    sv3->identify();
+
+    std::optional<SmallSub> o4 = sv3.or_else<SmallSub>([]() { return std::optional<SmallSub>(-1); });
+    assert(o4 && o4->y == 4);
+
+    // Test reset
+    sv2.emplace<SmallSub>(5);
+    assert(sv2);
+    sv2.reset();
+    assert(!sv2);
+
+    auto bv = make_polymorphic_value<BigSub>();
+    bv->identify();
+
+    // Move only testing
+    polymorphic_value<MoveOnly> mv;
+    mv.emplace<MoveOnly>();
+    // auto mv2 = mv; As T is not copyable neither is polymorphic_value<T>
+
+    sv.emplace<MoveOnly>();
+    try {
+        sv2 = sv;       // Runtime error as T but not U is copyable.
+    }
+    catch (std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+
+    try {
+        sv3.value<BigSub>();        // Fails
+    }
+    catch (std::exception& ex) {
+        std::cout << ex.what() << std::endl;
+    }
+
+}
