5912551424a2bdfb476bc7f284a0a7526aef662c
Bengt Gustafsson <bengt.gustafsson@beamways.com>
2022-09-18 15:33:33 +0200

    Initial version. Some writing in readme.

--- README.md
+++ README.md
@@ -1,2 +1,69 @@
 # PolymorphicValue
+
 Variation of std::polymorphic_value (P0201) with SBO support and options
+
+## Differences
+
+In contrast with the implementation at [https://github.com/jbcoe/polymorphic_value] as described by 
+[https://www.open-std.org/jtc1/sc22/wg21/docs/papers/2016/p0201r1.pdf] this implementation:
+
+- Has a SBO buffer option to avoid heap allocations
+
+- Does not risk slicing at copy by not allowing construction from U* or U& as such pointers or references could
+  potentially point at further subclasses. In P0201 this is a runtime check relying on dynamic_cast.
+
+- Does not support copying between polymorphic_value<T> and polymorphic_value<W> even if T and W have a inheritance relationship
+
+- Does not need heap storage for control blocks
+
+- Does not support allocators (P0201 does not either, but the implementation does).
+
+- Has options to prevent copying and even moving, even if T allows it.
+
+- Has an option to prevent heap allocation, in which case it is a compile error if U does not fit the SBO buffer.
+
+- Has an option to set a higher alignment than T's and if a  has a larger alignment than set this is a compile error.
+
+Note: Not all of these features are available in the first version.
+
+Warning: This initial version does not take the alignment requirement of T into account, and will fail if it is larger than that of
+a pointer.
+
+## Description
+
+A by value container of one object of any subclass of T with SBO for objects of small enough size. The API is close to unique_ptr.
+The semantics is also close but with the difference that references to the data are not preserved when the object is moved. Another
+difference is that the object has copy semantics if T (and U) has. Note that if T has copy semantics but U doesn't performing a copy
+will cause a runtime error.
+
+It would be beneficial to have a variant which never allocates on the heap and causes compile time error if the actual object is too
+large. Due to the cast functionality it would however be possible that these errors could crop up at run time anyway.
+
+The embedded handler object is responsible for copying itself as well as the data depending on if SBO is in effect or not. There are
+some optimizations available which are not employed:
+
+- if assigning between equally typed contained objects the copy/move assignment could be called instead of destroy + copy/move
+construct.
+
+- we don't actually need both const and non-const get virtual methods in handler, a cast is sufficient.
+
+The static data member sbo_size is the resulting size of the SBO buffer. It is usually the same as SZ but if T itself is larger than
+SZ we know that the SBO buffer would never be possible to use, so sbo_size is set to 1 which forces the "big" handlers to be set up
+always and brings the size of the polymorphic_value down to two pointers. This reduces the need for manually tweaking the SZ value
+for different Ts. It would be possible to let sbo_size somehow depend on sizeof(T) but it is hard to come up with a reasonable
+formula for this as T may be anything from a large class and no subclass adds members to an empty class with beefy subclasses.
+Instead The SZ value is set to some reasonable value similar to that of std::string. Applications can of course use type aliases to
+create their own variants with some other default SZ.
+
+Note that it is not possible to assign or construct from a polymorphic_value<U> to a polymorphic_value<T> even if U is a subclass of
+T. This limitation is due to the complexities of handling the this pointer offsets in case the polymorphic_value<U> actually
+contains an object of a further subclass Z: While the handler imbued by polymorphic_value<U> in polymorphic_value<T> handles the
+conversion from Z to U correctly the further conversion from U to T can't be handled without extra unbounded data. Some limited
+support such as "only without multiple inheritance" or "only without virtual inheritance" could be added but it would have size and
+speed cost.
+
+Note that it is also not possible to assign between polymorphic_values with different SZ values as this would potentially require
+copying the data from an external to internal storage or vice versa. As the handler of the source imbues itself on the destination
+this fails if the destination has a different SZ. Workarounds were tried but were not successful without incurring at least an extra
+virtual call and an if when accessing the underlying data, which seems too costly for such a rarely usable feature.
+
---
+++ polymorphic_value.h
@@ -0,0 +1,272 @@
+/*
+
+Test implementation of a polymorphic_value class for possible introduction into the C++ standard library.
+
+This software is provided under the MIT license:
+
+Copyright 2022 Bengt Gustafsson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files
+(the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify,
+merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Further information at: https://opensource.org/licenses/MIT.
+
+*/
+
+
+
+#pragma once
+
+#include <memory>
+#include <type_traits>
+#include <stdexcept>
+#include <utility>
+#include <optional>
+
+template<typename T, size_t SZ = 64> class polymorphic_value {
+public:
+    const static size_t sbo_size = SZ >= sizeof(T) ? SZ : 1;
+    polymorphic_value() {}
+    polymorphic_value(std::nullopt_t) {}
+    polymorphic_value(const polymorphic_value& src) requires std::is_copy_constructible_v<T> {
+        src.m_handler.imbue_handler(m_handler);
+        src.m_handler.copy(m_data, src.m_data);
+    }
+    polymorphic_value(polymorphic_value&& src) requires std::is_move_constructible_v<T> {
+        src.m_handler.imbue_handler(m_handler);
+        src.m_handler.move(m_data, src.m_data);
+        src.reset();
+    }
+    template<typename U, typename... Args> polymorphic_value(std::in_place_type_t<U>, Args&&... args)  requires std::is_base_of_v<T, U> {
+        emplace<U>(std::forward<Args>(args)...);
+    }
+
+    ~polymorphic_value() {
+        m_handler.destroy(m_data);
+    }
+
+    // TODO: To handle the odd case that a T is copy-assignable but not copy-constructible or vice versa
+    // handler would actually need to have both copy_assign and copy_contruct. Same for move. 
+    // Note that this can only be done if the embedded U of both me and src are the same, otherwise this destroy/construct sequence must be done anyway.
+    polymorphic_value& operator=(const polymorphic_value& src) requires std::is_copy_constructible_v<T> {
+        if (this == &src)
+            return *this;
+
+        m_handler.destroy(m_data);
+        src.m_handler.imbue_handler(m_handler);
+        src.m_handler.copy(m_data, src.m_data);
+        return *this;
+    };
+
+    polymorphic_value& operator=(polymorphic_value&& src) requires std::is_move_constructible_v<T> {
+        if (this == &src)
+            return *this;
+
+        m_handler.destroy(m_data);
+        src.m_handler.imbue_handler(m_handler);
+        src.m_handler.move(m_data, src.m_data);
+        src.reset();
+        return *this;
+    };
+
+    // Create object of subclass U of T, or by default a T.
+    template<typename U = T, typename... Args> void emplace(Args&&... args) {
+        m_handler.destroy(m_data);
+        if constexpr (sizeof(U) <= sbo_size) {
+            new(&m_handler) small_handler<U>;
+            std::construct_at(reinterpret_cast<U*>(m_data.m_bytes), std::forward<Args>(args)...);
+        }
+        else {
+            new(&m_handler) big_handler<U>;
+            std::construct_at(&m_data.m_ptr, std::make_unique<U>(std::forward<Args>(args)...));
+        }
+    }
+
+    // Get rid of a stored object, resetting the handler so that no double delete occurs later and so that operator bool returns false.
+    void reset() { m_handler.destroy(m_data); new(&m_handler) handler_base; }
+
+    operator bool() const { return get() != nullptr; }
+
+    // Access stored object. This enabled the pattern dynamic_cast<U*>(v.get()) to check and access the data.
+    // Without it
+    //    v.has_value<U>() ? static_cast<U*>(&*v) : nullptr
+    // would have to be used which seems too ugly. The more classical:
+    //     dynamic_cast<U*>(&*v);
+    // does not work without UB as operator* would return "struct at null" but maybe that's not a big deal.
+    //
+    // I would rather add a get() to optional, except that it is less useful there as there is only one null condition so maybe not.
+    //
+    T* get() { return m_handler.get(m_data); }
+    const T* get() const { return m_handler.get(m_data); }
+
+    T& operator*() { return *get(); }
+    const T& operator*() const { return *get(); }
+
+    T* operator->() { return get(); }
+    const T* operator->() const { return get(); }
+
+    // std::optional API
+    // Maybe a holds_alternative<U> from variant is more appropriate? But viewing different subclasses as alternatives seems a bit
+    // misleading.
+    bool has_value() const { return *this; }
+    template<typename U> bool has_value() const { return dynamic_cast<const U*>(get()) != nullptr; }
+
+    // Note: Skip rvalue versions for now.
+    template<typename U = T> U& value() {
+        U* ret = dynamic_cast<U*>(get());
+        if (ret == nullptr)
+            throw std::bad_optional_access();
+        return *ret;
+    }
+    template<typename U = T> const U& value() const {
+        U* ret = dynamic_cast<const U*>(get());
+        if (ret == nullptr)
+            throw std::bad_optional_access();
+        return *ret;
+    }
+    template<typename U = T, typename V> U value_or(V&& default_value) const {
+        const U* ret = dynamic_cast<const U*>(get());
+        if (ret != nullptr)
+            return *ret;
+        else
+            return static_cast<U>(std::forward<V>(default_value));
+    }
+
+    // Note: and_then requires F to return optional<X> or polymorphic_value<X> for some type X.
+    // This requirement is from optional, but in fact is quite strange, it could be any return type with a value-constructed
+    // default, such as a pointer.
+    template<typename U = T, typename F> auto and_then(F&& f) {
+        using R = std::remove_cvref_t<std::invoke_result_t<F, U&>>; // R must be some optional type.
+        if (has_value<U>())
+            return f(*static_cast<U*>(get()));
+        else
+            return R();
+    }
+    template<typename U = T, typename F> auto and_then(F&& f) const {
+        using R = std::remove_cvref_t<std::invoke_result_t<F, const U&>>;
+        if (has_value<U>())
+            return f(*static_cast<const U*>(get()));
+        else
+            return R();
+    }
+
+    // transform wraps the return value of F in an optional.
+    template<typename U = T, typename F> auto transform(F&& f) {
+        using R = std::remove_cvref_t<std::invoke_result_t<F, U&>>;
+        if (has_value<U>())
+            return std::optional<R>(f(*static_cast<U*>(get())));
+        else
+            return std::optional<R>();
+    }
+    template<typename U = T, typename F> auto transform(F&& f) const {
+        using R = std::remove_cvref_t<std::invoke_result_t<F, const U&>>;
+        if (has_value<U>())
+            return std::optional<R>(f(*static_cast<const U*>(get())));
+        else
+            return std::optional<R>();
+    }
+
+    // or_else requires F to return some optional.
+    template<typename U = T, typename F> auto or_else(F&& f) {
+        using R = std::remove_cvref_t<std::invoke_result_t<F>>;
+        if (has_value<U>())
+            return R(*static_cast<U*>(get()));
+        else
+            return f();
+    }
+    template<typename U = T, typename F> auto or_else(F&& f) const {
+        using R = std::remove_cvref_t<std::invoke_result_t<F>>;
+        if (has_value<U>())
+            return R(*static_cast<const U*>(get()));
+        else
+            return f();
+    }
+
+private:
+    union data {
+        data() : m_ptr(nullptr) {}
+        ~data() {}
+
+        std::byte m_bytes[sbo_size];
+        std::unique_ptr<T> m_ptr;
+    };
+
+    // Note: handler_base is not abstract, instead it is used for empty objects.
+    struct handler_base {
+        virtual void imbue_handler(handler_base& dest) const { new(&dest) handler_base; }
+
+        virtual T* get(data& d) const { return nullptr; }
+        virtual const T* get(const data& d) const { return nullptr; }
+
+        virtual void copy(data& dest, const data& src) const {}
+        virtual void move(data& dest, data& src) const {}
+        virtual void destroy(data& d) const {}
+    };
+    
+    template<typename U> struct small_handler final : public handler_base {
+        void imbue_handler(handler_base& dest) const override { new(&dest) small_handler<U>; }
+
+        T* get(data& d) const override { return static_cast<T*>(reinterpret_cast<U*>(d.m_bytes)); }
+        const T* get(const data& d) const override { return static_cast<const T*>(reinterpret_cast<const U*>(d.m_bytes)); }
+
+        void copy(data& dest, const data& src) const override {
+            if constexpr (std::is_copy_constructible_v<U>)
+                std::construct_at<U>(reinterpret_cast<U*>(dest.m_bytes), *reinterpret_cast<const U*>(src.m_bytes));
+            else
+                throw std::runtime_error("Tried to copy an object of move only subclass in a polymorphic_value<T> where T is copyable");
+        }
+        
+        void move(data& dest, data& src) const override {
+            if constexpr (std::is_move_constructible_v<U>)
+                std::construct_at<U>(reinterpret_cast<U*>(dest.m_bytes), std::move(*reinterpret_cast<U*>(src.m_bytes)));
+            else
+                throw std::runtime_error("Tried to move an object of immovable subclass in a polymorphic_value<T> where T is movable");
+        }
+
+        void destroy(data& d) const override { std::destroy_at(reinterpret_cast<U*>(d.m_bytes)); }
+    };
+    
+    template<typename U> struct big_handler final : public handler_base {
+        void imbue_handler(handler_base& dest) const override { new(&dest) big_handler<U>; }
+
+        T* get(data& d) const override { return d.m_ptr.get(); }
+        const T* get(const data& d) const override { return d.m_ptr.get(); }
+
+        void copy(data& dest, const data& src) const override { 
+            if constexpr (std::is_copy_constructible_v<U>)
+                std::construct_at(&dest.m_ptr, std::make_unique<U>(static_cast<const U&>(*src.m_ptr)));
+            else
+                throw std::runtime_error("Tried to copy an object of move only subclass in a polymorphic_value<T> where T is copyable");
+        }
+        void move(data& dest, data& src) const override {
+            if constexpr (std::is_move_constructible_v<U>)
+                std::construct_at(&dest.m_ptr, std::move(src.m_ptr));
+            else
+                throw std::runtime_error("Tried to move an object of immovable subclass in a polymorphic_value<T> where T is movable");
+        }
+
+        void destroy(data& d) const override { d.m_ptr = nullptr; }
+    };
+
+    handler_base m_handler;
+    data m_data;
+};
+
+template<typename T, typename U = T, typename... Args> polymorphic_value<T> make_polymorphic_value(Args&&... args)
+{
+    return polymorphic_value<T>(std::in_place_type<U>, std::forward<Args>(args)...);
+}
+
+template<typename T, size_t SZ, typename U = T, typename... Args> polymorphic_value<T, SZ> make_polymorphic_value(Args&&... args)
+{
+    return polymorphic_value<T, SZ>(std::in_place_type<U>, std::forward<Args>(args)...);
+}
